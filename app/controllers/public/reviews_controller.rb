ae227fd04365c942f93176e61914a07cff7d1892
masashi8081 <126481445+masashi8081@users.noreply.github.com>
2023-07-11 09:15:37 +0000

    review

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,5 +1,7 @@
 class Public::ReviewsController < ApplicationController
   def new
+     @review = Review.new
+     
   end
 
   def show
@@ -9,5 +11,42 @@ class Public::ReviewsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.customer_id = current_customer.id
+    if @review.save
+      redirect_to review_path(@review), notice: "You have created book successfully."
+    else
+      @reviews = Review.all
+      render 'index'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rate, :comment)
+  end
+
+  def read(result)
+    title = result["title"]
+    isbn = result["isbn"]
+    label = result["label"]
+    hardware = result["hardware"]
+    salesDate = result["salesDate"]
+    mediumImageUrl = result["mediumImageUrl"]
+    largeImageUrl = result["largeImageUrl"]
+
+    {
+      title: title,
+      isbn: isbn,
+      label: label,
+      hardware: hardware,
+      salesDate: salesDate,
+      mediumImageUrl: mediumImageUrl,
+      largeImageUrl: largeImageUrl
+    }
   end
 end
