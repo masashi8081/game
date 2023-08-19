84b81ba4c95646ade90ad164df98d3435f13b276
masashi8081 <126481445+masashi8081@users.noreply.github.com>
2023-08-19 06:54:23 +0000

    images

--- db/migrate/20230707122215_rename_mediumimage_url_column_to_games.rb
+++
@@ -1,5 +0,0 @@
-class RenameMediumimageUrlColumnToGames < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :games, :mediumimage_url, :mediumImageUrl
-  end
-end
--- db/migrate/20230707122541_rename_largeimage_url_column_to_games.rb
+++
@@ -1,5 +0,0 @@
-class RenameLargeimageUrlColumnToGames < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :games, :largeimage_url, :largeImageUrl
-  end
-end
